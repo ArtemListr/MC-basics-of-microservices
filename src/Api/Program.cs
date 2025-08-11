79f05a4bcef8fa300e48d33a89ec34c404822b99
Artem Listratov <listratovartem99@gmail.com>
2025-08-11 11:38:09 +0700

    002 Правки профилей доступа, обработка эндпоинтов

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/hello", () => "Hello World!"); // эндпоинт на котором выведется "Hello world!"
+app.MapGet("/foo", () => "foo");
+app.MapGet("/bar", () => "foo");
+app.MapGet("/bar1", () => "foo");
 
 app.Run();
