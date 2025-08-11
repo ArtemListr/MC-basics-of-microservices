c30840e8da34be9c64ccf47eb8b3e5bee95d81dd
Artem Listratov <listratovartem99@gmail.com>
2025-08-11 11:54:53 +0700

    003 Установил зависимости, удалил тестовые эндпоинты

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -3,9 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/hello", () => "Hello World!"); // эндпоинт на котором выведется "Hello world!"
-app.MapGet("/foo", () => "foo");
-app.MapGet("/bar", () => "foo");
-app.MapGet("/bar1", () => "foo");
+
 
 app.Run();
